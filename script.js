8050e1d31778c481995da4643f2b0866dcdf2a23
lashasuxa <lashasukhashvili1995@gmail.com>
2023-01-24 00:00:48 -0500

    upload files

---
+++ script.js
@@ -0,0 +1,27 @@
+const input = "turpentine and turtules";
+
+const vowels = ["a", "e", "i", "o", "u"];
+
+const resultArray = [];
+
+for (let inputIndex = 0; inputIndex < input.length; inputIndex++) {
+  // console.log(`inputIndex is ${inputIndex}`)
+  if (input[inputIndex] === "e") {
+    resultArray.push(input[inputIndex]);
+  }
+  if (input[inputIndex] === "u") {
+    resultArray.push(input[inputIndex]);
+  }
+
+  for (let vowelIndex = 0; vowelIndex < vowels.length; vowelIndex++) {
+    // console.log (`vowelIndex is ${vowelIndex}`)
+    if (input[inputIndex] === vowels[vowelIndex]) {
+      // console.log(input[inputIndex])
+      resultArray.push(input[inputIndex]);
+    }
+  }
+}
+
+//console.log(resultArray)
+const resultSring = resultArray.join("").toUpperCase();
+console.log(resultSring);
